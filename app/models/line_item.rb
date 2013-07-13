3339d73c7260ec9205600dd06b466f60c7402c29
Marcos Mariano <marcomariano@my.smccd.edu>
2013-07-12 21:54:32 -0700

    gambiarra

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,11 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :cart_id, :product_id, :product
+  attr_accessible :cart_id, :product_id, :product, :quantity
+  before_validation :quantitizer
+  
+  def quantitizer
+    self.quantity ||= 1    
+  end
+  
   
   #relations
   belongs_to :product
